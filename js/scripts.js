4ed96f2adca28c4bc35943aed2040bedffad825c
DeveloperAbdullahi <developer.abdullahihnur@gmail.com>
2021-05-11 13:06:43 +0300

    add function to display the date as a string

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ function showDateTime() {
         d = dateTime.getDate();
        var bsd = dateTime.getDay();
       
-
+        // if function to display the right string for the day
         if (bsd <= 1) {
             sd = "Monday";
         } else if (bsd <= 2) {
@@ -44,6 +44,43 @@ function showDateTime() {
         } else if (bsd <= 0) {
             sd = "Sunday";
         }
+
+        // get month and store to var
+        sm = dateTime.getMonth();
+        // if function to display the right string for the month
+        if (sm <= 0) {
+            m = "January";
+        } else if (sm <= 1) {
+            m = "February";
+        } else if (sm <= 2) {
+            m = "March";
+        } else if (sm <= 3) {
+            m = "April";
+        } else if (sm <= 4) {
+            m = "May";
+        } else if (sm <= 5) {
+            m = "June";
+        } else if (sm <= 6) {
+            m = "July";
+        } else if (sm <= 7) {
+            m = "August";
+        } else if (sm <= 8) {
+            m = "September";
+        } else if (sm <= 9) {
+            m = "October";
+        } else if (sm <= 10) {
+            m = "November";
+        } else if (sm <= 11) {
+            m = "December";
+        }
+        
+        y = dateTime.getFullYear();
+        console.log(y);
+
+        myDate.innerHTML = d + " " + m + " " + y;
+
+        
+    
     
 
     }
